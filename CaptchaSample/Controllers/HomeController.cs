8e543a66881442e3ee28306e829b54be1448a29e
DESKTOP-2NLB9QJ\Krkt <mustafasacli70@gmail.com>
2020-09-12 16:11:48 +0300

    first commit.

---
+++ CaptchaSample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaptchaSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult generateCaptcha()
+        {
+            System.Drawing.FontFamily family = new System.Drawing.FontFamily("Arial");
+            CaptchaImage img = new CaptchaImage(150, 50, family);
+            string text = img.CreateRandomText(4) + " " + img.CreateRandomText(3);
+            img.SetText(text);
+            img.GenerateImage();
+            img.Image.Save(Server.MapPath("~") + this.Session.SessionID.ToString() + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            Session["captchaText"] = text;
+            return Json(this.Session.SessionID.ToString() + ".png?t=" + DateTime.Now.Ticks, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
